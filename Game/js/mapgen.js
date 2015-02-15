0b882d4c50f8952d99ca41f7706c5c2066b75b8e
Mikalai Eutsikhieu <Mikalai_Eutsikhieu@epam.com>
2015-02-15 17:28:52 +0300

    Version 0.8.9

--- Game/js/mapgen.js
+++ Game/js/mapgen.js
@@ -3,9 +3,9 @@
 $.widget("ui.mapGen", {
     options : {
         scale : 1.5,
-        howMuch : 8,
+        howMuch : 5,
         border : {
-            color : 'rgba(0,0,0,0.5)',
+            color : 'rgba(0,0,0,0.4)',
             thick : 1,
         },
         fill : {
@@ -20,31 +20,46 @@ $.widget("ui.mapGen", {
         console.log(xside);
         var yside = (xside / this.options.scale);
         var sidesize = (xside / 2);
-        
-       
-        
+
         //understand y rows
-        var yRow = (thisHeight / yside).toFixed(0);
+        var yRow = (thisHeight / yside).toFixed(0)*2 - 2;
         var xRow = this.options.howMuch;
         
-        console.log('First polygon points="'+pointOne+' '+pointTwo+' '+pointThree+' '+pointFour+' '+pointFive+' '+pointSix+'"');
-        console.log('Total polygons: x="'+xRow+'" y="'+yRow+'"');
+        //understand free space 
+        var freeSpace = (thisWidth - (((this.options.howMuch*4) + ((this.options.howMuch-1)*2))*sidesize)).toFixed(0);
+        var correction = freeSpace/2;
+
+        console.log('Total polygons: x="'+xRow+'" y="'+yRow+'"' + ' Free Space:'+freeSpace);
         
-        for (var i = 0; i < this.options.howMuch; i++) {
-            var pointOne = [sidesize + (sidesize*6*i), yside];
-            var pointTwo = [0 + (sidesize*6*i), (yside/2)];
-            var pointThree = [sidesize + (sidesize*6*i), 0];
-            var pointFour = [(xside + sidesize) + (sidesize*6*i), 0];
-            var pointFive = [(xside + (sidesize*2 + (sidesize*6*i))), yside/2];
-            var pointSix = [(xside + sidesize) + (sidesize*6*i), yside];
-        
-        
-            d3.select('svg')
-              .append('polygon')
-              .attr('points', pointOne+' '+pointTwo+' '+pointThree+' '+pointFour+' '+pointFive+' '+pointSix);
-              
+        for (var iz = 0; iz < yRow; iz++) {
+            if (iz%2 == 0) {//четное число, основные строки 0, 2, 4, 6 и тд
+                for (var i = 0; i < this.options.howMuch; i++) {
+                    var pointOne = [sidesize + (sidesize*6*i) + correction, yside + (yside*iz)/2];
+                    var pointTwo = [0 + (sidesize*6*i) + correction, (yside/2) + (yside*iz)/2];
+                    var pointThree = [sidesize + (sidesize*6*i) + correction, 0 + (yside*iz)/2];
+                    var pointFour = [(xside + sidesize) + (sidesize*6*i) + correction, 0 + (yside*iz)/2];
+                    var pointFive = [(xside + (sidesize*2 + (sidesize*6*i))) + correction, yside/2 + (yside*iz)/2];
+                    var pointSix = [(xside + sidesize) + (sidesize*6*i) + correction, yside + (yside*iz)/2];
+                
+                    d3.select('svg')
+                      .append('polygon')
+                      .attr('points', pointOne+' '+pointTwo+' '+pointThree+' '+pointFour+' '+pointFive+' '+pointSix);
+                }
+            } else { //нечетное число, строки 1, 3, 5, которые идут между основными
+                for (var i = 0; i < (this.options.howMuch-1); i++) {
+                    var pointOne = [sidesize + (sidesize*6*i) + sidesize*3 + correction, yside + (yside*iz/2)];
+                    var pointTwo = [0 + (sidesize*6*i) + sidesize*3 + correction, (yside/2) + (yside*iz)/2];
+                    var pointThree = [sidesize + (sidesize*6*i) + sidesize*3 + correction, 0 + (yside*iz)/2];
+                    var pointFour = [(xside + sidesize) + (sidesize*6*i) + sidesize*3 + correction, 0 + (yside*iz)/2];
+                    var pointFive = [(xside + (sidesize*2 + (sidesize*6*i))) + sidesize*3 + correction, yside/2 + (yside*iz)/2];
+                    var pointSix = [(xside + sidesize) + (sidesize*6*i) + sidesize*3 + correction, yside + (yside*iz)/2];
+                
+                    d3.select('svg')
+                      .append('polygon')
+                      .attr('points', pointOne+' '+pointTwo+' '+pointThree+' '+pointFour+' '+pointFive+' '+pointSix);
+                }
+            }
         }
-        
         if (this.options.fill.color == 'none') {
             d3.selectAll('polygon')
                 .attr('fill','rgba(0,0,0,0');
